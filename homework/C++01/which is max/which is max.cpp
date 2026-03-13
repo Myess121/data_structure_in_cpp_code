2efe127ea8ea903a688a3a22458f52baf36b9a06
Myess121 <mengdeyuan0537@163.com>
2026-03-13 20:40:08 +0800

    求三个数中的量大值

---
+++ homework/C++01/which is max/which is max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,max;
+    cin>>a>>b>>c;
+    max = a;
+    if(a < b){
+        max = b;
+    }
+    if(max < c)
+    {
+        max = c;
+    }
+    cout <<"Maximum:"<<max<<endl;
+    return 0;
+
+}
